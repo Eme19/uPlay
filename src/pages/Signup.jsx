36a8e22dc2fba743af68ffcc06670bddc59317a6
“Emeka <ngwe7236@gmail.com>
2024-04-10 04:52:20 +0200

    update

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -13,7 +13,7 @@ const { Option } = Select;
 
 // const API_URL = process.env.REACT_APP_API_URL;
 
-const API_URL = process.env.REACT_APP_API_URL || "https://uplay-server.onrender.com";
+const API_URL = process.env.REACT_APP_API_URL;
 console.log("API URL:", API_URL); 
 
 function Signup() {
